2556503394a2a09b4e16f760b2a3bb0a5c2461df
安河内 <ryuji.yasu@gmail.com>
2021-08-26 17:49:45 +0900

    reactカレンダーおよび、componentのスタイル適用方法の確認 #5

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import Calendar from 'react-calendar'
+import Modal from 'react-modal';
+import Infomation from './Infomation';
+import 'react-calendar/dist/Calendar.css';
+
+const styles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+
+  Modal.setAppElement('#app');
 
 function App() {
-    return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+  
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function afterOpenModal() {
+      // references are now sync'd and can be accessed.
+      subtitle.style.color = '#f00';
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
 
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
+
+    const [value, onChange] = useState(new Date());
+
+    return (
+        <div>
+            <div className="container">
+                <div className="m-auto">
+                    <Calendar 
+                        onChange={onChange}
+                        value={value}
+                    />
+
                 </div>
+                <button onClick={openModal}>Open Modal</button>
+                <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={styles}
+                contentLabel="Example Modal"
+                >
+                    <Infomation></Infomation>
+                    
+                </Modal>
             </div>
+            
         </div>
     );
 }
+
 
 export default App;
 
 if (document.getElementById('app')) {
     ReactDOM.render(<App />, document.getElementById('app'));
 }
+
